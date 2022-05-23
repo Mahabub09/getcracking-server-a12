8671c3016f837d1e8de7e3709ec6ca29b6606e7d
mahbub09 <mahabub895alam@gmail.com>
2022-05-24 01:30:06 +0600

    tools collection

--- index.js
+++ index.js
@@ -9,3 +9,36 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.htfjo.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const toolsCollection = client.db('sinewy_tools').collection('tools');
+
+        app.get('/tool', async (req, res) => {
+            const query = {};
+            const cursor = toolsCollection.find(query);
+            const tools = await cursor.toArray();
+            res.send(tools);
+        });
+
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Running Sinewy Tools Server');
+});
+
+app.listen(port, () => {
+    console.log('Listening to port', port);
+})
