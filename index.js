8ad514b0ecb53e06def1772c7b7f8d802f66b121
mahbub09 <mahabub895alam@gmail.com>
2022-05-26 03:22:21 +0600

    cant do

--- index.js
+++ index.js
@@ -14,6 +14,22 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.htfjo.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
 async function run() {
     try {
         await client.connect();
@@ -65,6 +81,7 @@ async function run() {
 
         app.get('/orders', async (req, res) => {
             const email = req.query.email;
+
             const query = { email: email }
             const cursor = orderCollection.find(query);
             const orders = await cursor.toArray();
@@ -75,6 +92,10 @@ async function run() {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
             res.send(result);
+        })
+        app.get('/user', async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users)
         })
 
         app.put('/user/:email', async (req, res) => {
